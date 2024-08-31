4a66d4fccd9bcb365cad917143d890353c57e636
Kumaravel <kumar.okm1995@gmail.com>
2024-08-31 22:35:58 +0530

    Github repositry link icon added

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -1,14 +1,16 @@
 <script>
-    import { appConfig } from '../config';
-    import { goto } from '$app/navigation';
+	import { appConfig } from '../config';
+	import { goto } from '$app/navigation';
 	import { Button } from '$lib/components/ui/button';
 	import Sun from 'lucide-svelte/icons/sun';
 	import Moon from 'lucide-svelte/icons/moon';
 	import { onMount } from 'svelte';
+	import Github from '$lib/assets/github.svg';
+	import GithubWhite from '$lib/assets/github-white.svg';
 
-    const { appName } = appConfig;
+	const { appName } = appConfig;
 
-    let isDarkMode = false;
+	let isDarkMode = false;
 
 	onMount(async () => {
 		const prefersDarkScheme = window.matchMedia('(prefers-color-scheme: dark)').matches;
@@ -25,49 +27,67 @@
 				isDarkMode = true;
 				document.documentElement.classList.add('dark');
 			} else {
-        document.documentElement.classList.remove('dark');
-      }
+				document.documentElement.classList.remove('dark');
+			}
 		});
 	});
 
-    // Check if the user has set a dark mode preference
-    if (typeof window !== 'undefined') {
-        isDarkMode = localStorage.getItem('darkMode') === 'true';
-        if (isDarkMode) {
-            document.documentElement.classList.add('dark'); 
-        }
-    }
-    function toggleDarkMode() {
-        isDarkMode = !isDarkMode;
-        
-        if (typeof window !== 'undefined') {
-          localStorage.setItem('darkMode', isDarkMode ? 'true' : 'false');
-          document.documentElement.classList.toggle('dark', isDarkMode);
-        }
-    }
-  </script>
-  
-  <nav>
-    <h1 on:click={() => goto('/')}>{appName}</h1>
-	<Button on:click={toggleDarkMode}>
-		{#if isDarkMode}
-			<Sun className="mr-2 h-4 w-4" />
-		{:else}
-			<Moon className="mr-2 h-4 w-4" />
-		{/if}
-		{isDarkMode ? ' Light Mode' : ' Dark Mode'}
-	</Button>
-  </nav>
-  
-  <style>
-    nav {
-      display: flex;
-      align-items: center;
-      justify-content: space-between;
-    }
-  
-    h1 {
-      margin: 0;
-      cursor: pointer;
-    }
-  </style>
+	// Check if the user has set a dark mode preference
+	if (typeof window !== 'undefined') {
+		isDarkMode = localStorage.getItem('darkMode') === 'true';
+		if (isDarkMode) {
+			document.documentElement.classList.add('dark');
+		}
+	}
+	function toggleDarkMode() {
+		isDarkMode = !isDarkMode;
+
+		if (typeof window !== 'undefined') {
+			localStorage.setItem('darkMode', isDarkMode ? 'true' : 'false');
+			document.documentElement.classList.toggle('dark', isDarkMode);
+		}
+	}
+</script>
+
+<nav>
+	<h1 on:click={() => goto('/')}>{appName}</h1>
+	<div class="right-contents">
+		<a target="_blank" href="https://github.com/kumaravel95/developer-utils">
+			{#if isDarkMode}
+				<img src={GithubWhite} alt="Github" />
+			{:else}
+				<img src={Github} alt="Github" />
+			{/if}
+		</a>
+		&nbsp;
+		<Button on:click={toggleDarkMode}>
+			{#if isDarkMode}
+				<Sun className="mr-2 h-4 w-4" />
+			{:else}
+				<Moon className="mr-2 h-4 w-4" />
+			{/if}
+			{isDarkMode ? ' Light Mode' : ' Dark Mode'}
+		</Button>
+	</div>
+</nav>
+
+<style>
+	nav {
+		display: flex;
+		align-items: center;
+		justify-content: space-between;
+	}
+
+	.right-contents {
+		display: flex;
+	}
+
+  .right-contents a {
+    padding-top: 2px;
+  }
+
+	h1 {
+		margin: 0;
+		cursor: pointer;
+	}
+</style>
